c01ffe3ac228ef96953a2140e15679924ceda68d
Alexandra Paramonova <alpar.wb@gmail.com>
2023-12-19 00:54:52 +0500

    fix: implement Stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _array = [];
+
+  push(element) {
+    this._array.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._array.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._array.slice(-1)[0];
   }
 }
 
