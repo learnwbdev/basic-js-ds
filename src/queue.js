1b507a8934d9b967941466eb4e5dece4956ee152
Alexandra Paramonova <alpar.wb@gmail.com>
2023-12-19 12:53:44 +0500

    fix: implement Queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  head = null;
+  tail = null;
+  size = 0;
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.size === 0) {
+      return undefined;
+    }
+    let currentElement = this.head;
+    let currentQueue = {value: currentElement.value, next: null};
+    let currentQueueNext = currentQueue;
+    while (currentElement.next !== null) {
+      currentElement = currentElement.next;
+      currentQueueNext.next = {value: currentElement.value, next: null};
+      currentQueueNext = currentQueueNext.next;
+    }
+    return currentQueue;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const newElement = new ListNode(value);
+    if (this.size === 0) {
+      this.head = newElement;
+    } else {
+      this.tail.next = newElement;
+    }
+    this.tail = newElement;
+    this.size += 1;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const topElement = this.size === 0 ? null : this.head.value;
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+      this.size = 0;
+    } else {
+      const topNextElement = this.head.next;
+      this.head = topNextElement;
+      this.size -= 1;
+    }
+    return topElement;
   }
 }
 
